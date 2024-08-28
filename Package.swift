5d4506d3f2b8c016fbb86c6c2988834c55660c9c
Daniel Falcão <35662363+daniellfalcao@users.noreply.github.com>
2024-08-28 10:42:36 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KOSMobile",
-            url: "https://github.com/daniellfalcao/kos-rs-fork/releases/download/0.0.19/KOSMobile.xcframework.zip",
-            checksum: "849fef7850a1430e6df1c5cbfdd9439790d0f11766ce147ac21852c71420615d"
+            url: "",
+            checksum: ""
         ),
     ]
 )
